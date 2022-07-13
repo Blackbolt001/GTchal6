561abcbb063420f53ee58539e90c07aa911f8e13
JoeG <jgossett73@gmail.com>
2022-07-13 05:19:03 -0400

    night push

--- assets/JavaScript/scripts.js
+++ assets/JavaScript/scripts.js
@@ -1,2 +1,7 @@
-var url = document.getElementsByClassName("inputValue");
-var appid = "139764592ad2853c5c9559c1e1dbe824";
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={139764592ad2853c5c9559c1e1dbe824}')
