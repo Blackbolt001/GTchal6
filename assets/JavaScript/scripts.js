2a7255387c3a785fd650b8ff41fec7a772ce5320
unknown <Jgossett73@gmail.com>
2022-07-29 17:53:44 -0400

    added button and changed layout again

--- assets/JavaScript/scripts.js
+++ assets/JavaScript/scripts.js
@@ -1,6 +1,11 @@
-   
-
-  
-
-
+   /*<div class="card">
+  <h5 class="card-title p-2">Date</h5>
+<div class ="card-body">
+    <h3 class="card-title">Location</h3>
+    <h2 class="card-title">Weather</h2>
+    <p class="card-text">Forcast</p>
+    <p class="card-text">Rain</p>
+</div>
+</div>
+</div>///*
 
